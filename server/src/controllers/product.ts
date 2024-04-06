64d00f95e14ffd309635680250ddb04bfdbf5a2b
anisameliawati <anisameliawati20@gmail.com>
2024-04-06 16:51:31 +0700

    add controllers

--- server/src/controllers/product.ts
+++ server/src/controllers/product.ts
@@ -3,25 +3,24 @@ import { Prisma } from "@prisma/client";
 import { prisma } from "..";
 
 export const productController = {
-  async addProduct(req: Request, res: Response, next: NextFunction) {
-    try {
-      const { productName, description, price, image_url } = req.body;
-      const newProduct: Prisma.ProductCreateInput = {
-        productName,
-        image_url,
-        price,
-        description,
-      };
-
-      await prisma.product.create({
-        data: newProduct,
-      });
-      res.send({
-        success: true,
-        message: "data berhasil ditambahkan",
-      });
-    } catch (error) {
-      next(error);
-    }
-  },
+  //   async addProduct(req: Request, res: Response, next: NextFunction) {
+  //     try {
+  //       const { productName, description, price, image_url } = req.body;
+  //       const newProduct: Prisma.ProductCreateInput = {
+  //         productName,
+  //         image_url,
+  //         price,
+  //         description,
+  //       };
+  //       await prisma.product.create({
+  //         data: newProduct,
+  //       });
+  //       res.send({
+  //         success: true,
+  //         message: "data berhasil ditambahkan",
+  //       });
+  //     } catch (error) {
+  //       next(error);
+  //     }
+  //   },
 };
