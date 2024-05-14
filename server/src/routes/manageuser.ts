0336fda708f5353f2df1322584c390670bafde1a
dimassamiaji <dimassamji@gmail.com>
2024-05-14 14:03:31 +0700

    update controller manage user

--- server/src/routes/manageuser.ts
+++ server/src/routes/manageuser.ts
@@ -3,12 +3,10 @@
 import express, { Router } from "express";
 import { manageUserController } from "../controllers/manageusers";
 import { verifyAdmin, verifyUser } from "../middlewares/auth-middleware";
-import { fileUploader } from "../middlewares/multer";
+
 export const route: Router = express.Router();
 route.get("/", manageUserController.getAllUsers);
 route.get("/:id", manageUserController.getUserById);
 route.patch("/:id", verifyUser, verifyAdmin, manageUserController.updateUser);
-
 route.post("/", verifyUser, verifyAdmin, manageUserController.createUser);
-
 route.delete("/:id", verifyUser, verifyAdmin, manageUserController.deleteUser);
