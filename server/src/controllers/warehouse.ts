8baaf31594fe9aaff9d889fa9328f63e36d7f238
dimassamiaji <dimassamji@gmail.com>
2024-05-21 12:32:43 +0700

    update warehouse

--- server/src/controllers/warehouse.ts
+++ server/src/controllers/warehouse.ts
@@ -1,136 +1,177 @@
 import { Request, Response, NextFunction } from "express";
 import { prisma } from ".."; // Ensure this path aligns with your Prisma client setup
 import { Prisma } from "@prisma/client";
+import { ReqUser } from "../middlewares/auth-middleware";
 
 export const warehouseController = {
-  // // Get all warehouses
-  // async getAllWarehouses(
-  //   req: Request,
-  //   res: Response,
-  //   next: NextFunction
-  // ): Promise<void> {
-  //   try {
-  //     const warehouses = await prisma.warehouse.findMany({
-  //       include: {
-  //         user: true, // Includes user details for warehouse admin
-  //         city: true,
-  //         province: true,
-  //       },
-  //     });
-  //     res.status(200).send(warehouses);
-  //   } catch (error) {
-  //     console.error("Error fetching warehouses:", error);
-  //     res.status(500).send("Failed to fetch warehouses");
-  //   }
-  // },
-  // // Get a single warehouse by ID
-  // async getWarehouseById(
-  //   req: Request,
-  //   res: Response,
-  //   next: NextFunction
-  // ): Promise<void> {
-  //   try {
-  //     const warehouse = await prisma.warehouse.findUnique({
-  //       where: { id: Number(req.params.id) },
-  //       include: {
-  //         user: true,
-  //         city: true,
-  //         province: true,
-  //       },
-  //     });
-  //     if (warehouse) {
-  //       res.status(200).send(warehouse);
-  //     } else {
-  //       res.status(404).send("Warehouse not found");
-  //     }
-  //   } catch (error) {
-  //     console.error("Error getting warehouse by ID:", error);
-  //     res.status(500).send("Error retrieving warehouse");
-  //   }
-  // },
-  // // Create a new warehouse
-  // async createWarehouse(
-  //   req: Request,
-  //   res: Response,
-  //   next: NextFunction
-  // ): Promise<void> {
-  //   try {
-  //     const {
-  //       warehouse_name,
-  //       user_id,
-  //       city_id,
-  //       province_id,
-  //       longtitude,
-  //       latitude,
-  //       location,
-  //     } = req.body;
-  //     const newWarehouse = await prisma.warehouse.create({
-  //       data: {
-  //         warehouse_name,
-  //         user: { connect: { id: user_id } },
-  //         city: { connect: { id: city_id } },
-  //         province: { connect: { id: province_id } },
-  //         longtitude,
-  //         latitude,
-  //         location,
-  //       },
-  //     });
-  //     res.status(201).send("Warehouse created successfully");
-  //   } catch (error) {
-  //     console.error("Error creating warehouse:", error);
-  //     res.status(500).send("Failed to create warehouse");
-  //   }
-  // },
-  // // Update an existing warehouse
-  // async updateWarehouse(
-  //   req: Request,
-  //   res: Response,
-  //   next: NextFunction
-  // ): Promise<void> {
-  //   try {
-  //     const {
-  //       warehouse_name,
-  //       user_id,
-  //       city_id,
-  //       province_id,
-  //       longtitude,
-  //       latitude,
-  //       location,
-  //     } = req.body;
-  //     const updatedWarehouse = await prisma.warehouse.update({
-  //       where: { id: Number(req.params.id) },
-  //       data: {
-  //         warehouse_name,
-  //         user: { connect: { id: user_id } },
-  //         city: { connect: { id: city_id } },
-  //         province: { connect: { id: province_id } },
-  //         longtitude,
-  //         latitude,
-  //         location,
-  //       },
-  //     });
-  //     res.status(200).send("Warehouse updated successfully");
-  //   } catch (error) {
-  //     console.error("Error updating warehouse:", error);
-  //     res.status(500).send("Failed to update warehouse");
-  //   }
-  // },
-  // // Delete a warehouse
-  // async deleteWarehouse(
-  //   req: Request,
-  //   res: Response,
-  //   next: NextFunction
-  // ): Promise<void> {
-  //   try {
-  //     await prisma.warehouse.delete({
-  //       where: { id: Number(req.params.id) },
-  //     });
-  //     res.status(200).send("Warehouse deleted successfully");
-  //   } catch (error) {
-  //     console.error("Error deleting warehouse:", error);
-  //     res.status(500).send("Failed to delete warehouse");
-  //   }
-  // },
+  async getWarehouses(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { city_name, province_name } = req.query;
+      const warehouse = await prisma.warehouse.findMany({
+        include: {
+          city: {
+            select: {
+              city_name: true,
+            },
+          },
+          user: {
+            select: {
+              id: true,
+              email: true,
+              first_name: true,
+              last_name: true,
+            },
+          },
+          province: {
+            select: {
+              province_name: true,
+            },
+          },
+        },
+        where: {
+          AND: [
+            {
+              city: {
+                city_name: {
+                  contains: String(city_name).toLowerCase(),
+                },
+              },
+            },
+            {
+              province: {
+                province_name: {
+                  contains: String(province_name).toLocaleLowerCase(),
+                },
+              },
+            },
+          ],
+        },
+      });
+      res.send({
+        success: true,
+        result: warehouse,
+        message: "Get Warehouse is success",
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async getwarehouseById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const warehouse = await prisma.warehouse.findUnique({
+        include: {
+          city: {
+            select: {
+              city_name: true,
+            },
+          },
+          user: {
+            select: {
+              id: true,
+              email: true,
+              first_name: true,
+              last_name: true,
+            },
+          },
+        },
+        where: {
+          id: Number(req.params.id),
+        },
+      });
+
+      res.send({
+        success: true,
+        result: warehouse,
+        message: "Get Warehouse by Id is success",
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async addWarehouse(req: ReqUser, res: Response, next: NextFunction) {
+    try {
+      const { warehouse_name, location, city, province, longtitude, latitude } =
+        req.body;
+      const newWarehouse: Prisma.WarehouseCreateInput = {
+        warehouse_name,
+        location,
+        city,
+        province,
+        longtitude,
+        latitude,
+        user: {
+          connect: {
+            id: req.user?.id,
+          },
+        },
+      };
+
+      await prisma.warehouse.create({
+        data: newWarehouse,
+      });
+      res.send({
+        success: true,
+        message: "Adding New Warehouse",
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async editwarehouse(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { warehouse_name, longtitude, latitude, location, city } = req.body;
+
+      const editwarehouse: Prisma.WarehouseUpdateInput = {
+        warehouse_name,
+        longtitude,
+        latitude,
+        location,
+        user: {
+          connect: {
+            id: Number(req.params.id),
+          },
+        },
+        city: {
+          connect: {
+            id: Number(city),
+          },
+        },
+      };
+
+      await prisma.warehouse.update({
+        data: editwarehouse,
+        where: {
+          id: Number(req.params.id),
+        },
+      });
+      res.send({
+        success: true,
+        message: "Edited",
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+  async deleteWarehouse(req: Request, res: Response, next: NextFunction) {
+    try {
+      const data: Prisma.WarehouseUncheckedUpdateInput = {
+        deleted_at: new Date(),
+      };
+      await prisma.warehouse.update({
+        data: data,
+        where: { id: Number(req.params.id) },
+      });
+      res.send({
+        success: true,
+        message: "SOFT DELETED",
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
 
   async assignWarehouseAdmin(req: Request, res: Response, next: NextFunction) {
     try {
